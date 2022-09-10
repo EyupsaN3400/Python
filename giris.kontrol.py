ea52a39408343db3d6c5c3c7b904b2beca2865ea
EyüpK <eyupkilic1901@gmail.com>
2022-09-10 21:19:00 +0300

    python entry Controlling
    Panel entry Controlling

---
+++ giris.kontrol.py
@@ -0,0 +1,16 @@
+email = 'Eyupk@gmail.com'
+password = 'eyupk'
+
+entry_email = input('emaili girin: ')
+entry_password = input('şifrenizi girin: ')
+
+
+if (email == entry_email):
+    if (password == entry_password):
+        print('giriş başarılı yönlendiriliyorsunuz')
+
+    else:
+        print('şifreniz yanlış tekrar deneyin')
+
+else:
+    print('emailiniz yanlış lütfen tekrar deneyin')
